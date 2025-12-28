4509d3d1d2116c7a7ae0d082740d251467fa28a3
Mahabub Rahman <mahabubrahman618@gmail.com>
2025-12-28 17:40:34 +0600

    add routing for EventTimetableComponent and update app layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventTimetableComponent } from './event-timetable/event-timetable.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EventTimetableComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
